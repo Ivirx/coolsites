a4e06bb80e75a96095da0ee18b27cb175fd74030
Ivirx <scorpianva@gmail.com>
2023-04-17 20:21:18 +0530

    accordion is done

--- accordion/script.js
+++ accordion/script.js
@@ -1,11 +1,24 @@
 const accordionHolder = document.querySelector('.accordion-holder');
 const accordions = accordionHolder.querySelectorAll('.accordion');
 
-accordionHolder.addEventListener('click', event => {
+function activateAccordion(event) {
     const clickedAccordion = event.target.closest('.accordion');
 
     if (!clickedAccordion) return;
 
-    accordions.forEach(accordion => accordion.classList.remove('accordion--active'));
-    clickedAccordion.classList.add('accordion--active');
+    accordions.forEach(accordion => accordion.classList.remove('active'));
+    clickedAccordion.classList.add('active');
+}
+
+accordionHolder.addEventListener('click', (event) => {
+    const clickedAccordion = event.target.closest('.accordion');
+
+    if (!clickedAccordion) return;
+
+    clickedAccordion.querySelector('button').focus();
 });
+
+accordionHolder.querySelectorAll('button').forEach(button => button.addEventListener('focus', activateAccordion));
+
+
+accordionHolder.querySelector('button').focus();
