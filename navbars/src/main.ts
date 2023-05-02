09834b4a01fcfb9c88455c74b0b5501facaf1161
Ivirx <scorpianva@gmail.com>
2023-05-02 18:28:06 +0530

    all navs works is done and is complete

--- navbars/src/main.ts
+++ navbars/src/main.ts
@@ -1,20 +1,31 @@
 import './style.scss';
 
-const horizontalNav = document.querySelector('.horizontal-nav') as HTMLElement;
-const horizontalNavIndicator = horizontalNav?.querySelector('.horizontal-nav__indicator') as HTMLElement;
+const container = document.querySelector('.container') as HTMLElement;
+const horizontalNavIndicator = container?.querySelector('.horizontal-nav__indicator') as HTMLElement;
+const verticalNavIndicator = container?.querySelector('.vertical-nav__indicator') as HTMLElement;
 
-if (horizontalNav)
-  horizontalNav.onclick = e => {
+if (container)
+  container.onclick = e => {
     const target = e.target as HTMLElement;
-    const validIcon = target.closest('[class*="icon-"]') as HTMLElement;
+    const validIcon = target.closest('[class*="icon"]') as HTMLElement;
 
     // If the target is not an icon, return
     if (!validIcon) return;
 
-    document.body.style.backgroundColor = `var(--theme-${validIcon.dataset.id})`;
-    if (horizontalNavIndicator) {
-      horizontalNavIndicator.classList.remove('animate');
-      horizontalNavIndicator.style.left = `calc(var(--padding-inline) + ((var(--svg-size) + var(--icon-gap)) * ${validIcon.dataset.id}))`;
-      setTimeout(() => horizontalNavIndicator.classList.add('animate'), 1);
+    const iconId = validIcon.dataset.id;
+
+    document.body.style.backgroundColor = `var(--theme-${iconId})`;
+
+    if (iconId && parseInt(iconId) < 5) {
+      if (horizontalNavIndicator) {
+        horizontalNavIndicator.classList.remove('animate');
+        horizontalNavIndicator.style.left = `calc(var(--padding-inline) + ((var(--svg-size) + var(--icon-gap)) * ${iconId}))`;
+        setTimeout(() => horizontalNavIndicator.classList.add('animate'), 1);
+      }
+    }
+    if (iconId && parseInt(iconId!) > 4) {
+      if (verticalNavIndicator) {
+        verticalNavIndicator.style.top = `calc(var(--padding-block) + ((var(--svg-size) + var(--icon-gap)) * ${parseInt(iconId) - 5}))`;
+      }
     }
   };
