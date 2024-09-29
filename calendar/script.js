7e865be95c4ba4a196d47b8fa84b3c4409a934a3
Vivek Kumar <scorpianva@gmail.com>
2024-09-29 12:41:32 +0530

    updated few things in calender

--- calendar/script.js
+++ calendar/script.js
@@ -10,11 +10,11 @@ window.onload = () => {
     for (let i = 0; i < 35; i++) {
         const dateWrapper = dateWrapperTemplate.content.cloneNode(true);
         const [s1, s2, s3] = dateWrapper.querySelectorAll('span');
-        s1.textContent = i + 1;
+        s1.textContent = '-';
         s1.style.transitionDelay = `${10 * i}ms`;
-        s2.textContent = i + 2;
+        s2.textContent = '-';
         s2.style.transitionDelay = `${10 * i}ms`;
-        s3.textContent = i + 3;
+        s3.textContent = '-';
         s3.style.transitionDelay = `${10 * i}ms`;
         dateGrid.appendChild(dateWrapper);
     }
@@ -31,9 +31,14 @@ window.onload = () => {
         monthEle.innerHTML = MONTHS[month];
         spanYear.innerHTML = year;
 
-        for (let i = 0; i < 35; i++) {
-            dates[(i + startDay) % 35].innerHTML = i + 1 > noOfDays ? '' : i + 1;
-        }
+        for (let i = 0; i < 35; i++)
+            dates[(i + startDay) % 35].innerHTML = i + 1 > noOfDays ? '-' : i + 1;
+
+
+        document.querySelectorAll('.today').forEach(today => today.classList.remove('today'));
+        const today = new Date();
+        if (today.getFullYear() === year && today.getMonth() === month)
+            dates[today.getDate() + startDay - 1].classList.add('today');
     }
 
     const nxt = document.querySelector('#nxt');
@@ -43,16 +48,16 @@ window.onload = () => {
         nxt.disabled = true;
         pre.disabled = true;
 
-        month = month + 1;
+        month++;
         if (month > 11) {
             month = 0;
             year++;
         }
         fillTheDates(year, month, document.querySelectorAll(".date-wrapper>span:last-child"), document.querySelector("#month-wrapper>span:last-child"));
 
-        dateWrappers.forEach(dateWrapper => {
-            dateWrapper.appendChild(dateWrapper.querySelector('span:first-child'));
-        });
+        dateWrappers.forEach(dateWrapper =>
+            dateWrapper.appendChild(dateWrapper.querySelector('span:first-child'))
+        );
         monthWrapper.appendChild(monthWrapper.querySelector('span:first-child'));
 
         nxt.disabled = false;
@@ -63,21 +68,25 @@ window.onload = () => {
         nxt.disabled = true;
         pre.disabled = true;
 
-        month = month - 1;
+        month--;
         if (month < 0) {
             month = 11;
             year--;
         }
         fillTheDates(year, month, document.querySelectorAll(".date-wrapper>span:first-child"), document.querySelector("#month-wrapper>span:first-child"));
 
-        dateWrappers.forEach(dateWrapper => {
-            dateWrapper.insertBefore(dateWrapper.querySelector('span:last-child'), dateWrapper.querySelector('span:first-child'));
-        });
+        dateWrappers.forEach(dateWrapper =>
+            dateWrapper.insertBefore(dateWrapper.querySelector('span:last-child'), dateWrapper.querySelector('span:first-child'))
+        );
         monthWrapper.insertBefore(monthWrapper.querySelector('span:last-child'), monthWrapper.querySelector('span:first-child'));
 
         nxt.disabled = false;
         pre.disabled = false;
     }
 
-    fillTheDates(year, month, document.querySelectorAll(".date-wrapper>span:nth-child(2)"), document.querySelector("#month-wrapper>span:nth-child(2)"));
+    document.querySelector('main').classList.add('load');
+    setTimeout(() => {
+        month--;
+        nxt.click();
+    }, 1400);
 }
